cb57f9907e587b3d111adbd791fdcbf9bab6169c
US18095196754\Admin555 <snakedrummm@gmail.com>
2021-10-13 14:14:49 +0300

    The first

---
+++ TestProje/TestKlass.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace TestProje
+{
+    public class TestKlass
+    {
+        IWebDriver driver = new ChromeDriver();
+        [SetUp]
+        public void Initialize()
+        {
+            driver.Navigate().GoToUrl("https://exe.ua/");
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            IWebElement element = driver.FindElement(By.Id("search_query"));
+            element.SendKeys("chuj,pizda");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+
+        }
+    }
+}
