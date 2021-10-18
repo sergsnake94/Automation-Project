bc1dc15747eb4f12c02f63154bf582567189a4eb
US18095196754\Admin555 <snakedrummm@gmail.com>
2021-10-18 21:46:03 +0300

    Add First Test

--- TestProje/TestKlass.cs
+++ TestProje/TestKlass.cs
@@ -2,31 +2,62 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Threading;
 
 namespace TestProje
 {
     public class TestKlass
     {
+       
+
         IWebDriver driver = new ChromeDriver();
+
+        public IWebDriver Driver { get => driver; set => driver = value; }
+
         [SetUp]
         public void Initialize()
         {
-            driver.Navigate().GoToUrl("https://exe.ua/");
+            Driver.Navigate().GoToUrl("https://exe.ua/");
         }
 
         [Test]
         public void ExecuteTest()
         {
-            IWebElement element = driver.FindElement(By.Id("search_query"));
-            element.SendKeys("chuj,pizda");
+            IWebElement btnKontacts = Driver.FindElement(By.XPath("(//a[@href='/ua/contacts/'])[1]"));
+            btnKontacts.Click();
+            IWebElement txtKontacts = Driver.FindElement(By.TagName("h1"));
+            string text = txtKontacts.Text;
+            Assert.AreEqual("Контакти", text, "String is not as expected");
+
+
+            IWebElement BtnDostavka = Driver.FindElement(By.XPath("(//a[@href='/ua/payment-and-delivery/'])[1]"));
+            BtnDostavka.Click();
+            IWebElement txtDostavka = Driver.FindElement(By.XPath("//h1"));
+            text = txtDostavka.Text;
+            Assert.AreEqual("Доставка і оплата", text, "String is not as expected");
+            
+
+
         }
 
         [TearDown]
         public void CleanUp()
         {
-            driver.Close();
+            
             //this is Sparta
 
+        }
+        [OneTimeSetUp]
+        public void Precondition()
+        {
+
+            Driver.Navigate().GoToUrl("https://exe.ua/");
+            Thread.Sleep(1000);
+        }
+      //[OneTimeTearDown]
+        public void Postcondition()
+        {
+            Driver.Close();
         }
     }
 }
